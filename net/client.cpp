e613d42859633b775838d7d7c1ef76755e43b1bf
wenbin <928568891@qq.com>
2019-02-11 18:43:08 +0800

    Signed-off-by: wenbin <928568891@qq.com>

---
+++ net/client.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h> //for gethostbyname
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX_BUF 4096
+#define SERVER_PORT 12138
+
+
+int main(int argc,char *argv[])
+{
+    int sockfd;//socket
+    char sendBuf[MAX_BUF],recvBuf[MAX_BUF];
+    int sendSize,recvSize;//用于记录记录发送和接收到数据的大小
+    struct hostent * host;
+    struct sockaddr_in servAddr;
+    char username[32];
+    char * p;
+
+    if(argc != 3)
+    {
+        perror("use: ./client [hostname] [username]");
+        exit(-1);
+    }
+    p=username;
+    strcpy(p,argv[2]);
+    printf("username:%s\n",username);
+    host=gethostbyname(argv[1]);
+    if(host==NULL)
+    {
+    　　perror("fail to get host by name.");
+        exit(-1);
+    }
+    printf("Success to get host by name ...\n");
+
+    //创建socket
+    if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)
+    {
+    　　perror("fail to establish a socket");
+    　　exit(1);
+    }
+    printf("Success to establish a socket...\n");
+    
+    /*init sockaddr_in*/
+    servAddr.sin_family=AF_INET;
+    servAddr.sin_port=htons(SERVER_PORT);
+    servAddr.sin_addr=*((struct in_addr *)host->h_addr);
+    bzero(&(servAddr.sin_zero),8);
+
+    /*connect the socket*/
+    if(connect(sockfd,(struct sockaddr *)&servAddr,sizeof(struct sockaddr_in))==-1)
+    {
+    　　perror("fail to connect the socket");
+    　　exit(1);
+    }
+    printf("Success to connect the socket...\n");
+
+    //send-recv
+    while(1)
+    {
+        printf("Input:");
+        fgets(sendBuf,MAX_BUF,stdin);
+        send(sockfd,sendBuf,strlen(sendBuf),0);
+        memset(sendBuf,0,sizeof(sendBuf));
+        recv(sockfd,recvBuf,MAX_BUF,0);
+        printf("Server:%s\n",recvBuf);
+        memset(recvBuf,0,sizeof(recvBuf));
+    }
+
+    return 0;
+}
--- net/main.cpp
+++ net/main.cpp
@@ -3,17 +3,105 @@
 
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
+#define SERVER_PORT 12138
+#define BACKLOG 20
+#define MAX_CON_NO 10
+#define MAX_DATA_SIZE 4096
 
-
-
-
-
-int main()
+int main(int argc,char *argv[])
 {
-	return 0;
+    struct sockaddr_in serverSockaddr,clientSockaddr;
+    char sendBuf[MAX_DATA_SIZE],recvBuf[MAX_DATA_SIZE];
+    int sendSize,recvSize;
+    int sockfd,clientfd;
+    int on=1;
+    int sinSize=0;
+    char username[32];
+
+    if(argc != 2)
+    {
+        printf("usage: ./server [username]\n");
+        exit(1);
+    }
+    strcpy(username,argv[1]);
+    printf("username:%s\n",username);
+
+    /*establish a socket*/
+    if((sockfd = socket(AF_INET,SOCK_STREAM,0))==-1)
+    {
+        perror("fail to establish a socket");
+        exit(1);
+    }
+    printf("Success to establish a socket...\n");
+
+    /*init sockaddr_in*/
+    serverSockaddr.sin_family=AF_INET;
+    serverSockaddr.sin_port=htons(SERVER_PORT);
+    serverSockaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    bzero(&(serverSockaddr.sin_zero),8);
+
+    setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on));
+
+    /*bind socket*/
+    if(bind(sockfd,(struct sockaddr *)&serverSockaddr,sizeof(struct sockaddr))==-1)
+    {
+        perror("fail to bind");
+        exit(1);
+    }
+    printf("Success to bind the socket...\n");
+
+    /*listen on the socket*/
+    if(listen(sockfd,BACKLOG)==-1)
+    {
+        perror("fail to listen");
+        exit(1);
+    }
+
+    /*accept a client's request*/
+    if((clientfd=accept(sockfd,(struct sockaddr  *)&clientSockaddr, &sinSize))==-1)
+    {
+        perror("fail to accept");
+        exit(1);
+    }
+    printf("Success to accpet a connection request...\n");
+    printf(" %s join in!\n",inet_ntoa(clientSockaddr.sin_addr));
+    while(1)
+    {
+        /*receive datas from client*/
+        if((recvSize=recv(clientfd,recvBuf,MAX_DATA_SIZE,0))==-1)
+        {
+            perror("fail to receive datas");
+            exit(1);
+        }
+        printf("%s\n",recvBuf);
+        memset(recvBuf,0,MAX_DATA_SIZE);
+
+        /*send datas to client*/
+        printf("Server:");
+        fgets(sendBuf,MAX_DATA_SIZE,stdin);
+        if((sendSize=send(clientfd,sendBuf,strlen(sendBuf),0))!=strlen(sendBuf))
+        {
+            perror("fail to send datas");
+            exit(1);
+        }
+        printf("Success to send datas\n");
+        memset(sendBuf,0,MAX_DATA_SIZE);
+    }
+
+    return 0;
 }
-
-
-
